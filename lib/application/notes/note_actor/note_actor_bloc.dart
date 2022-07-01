1542878d2ef1bfcb2576166aae2e94d62c9d95b1
Augusto Giavedoni <58192738+augiavedoni@users.noreply.github.com>
2022-07-01 12:33:36 -0300

    Implement note_watcher and note_actor BLoCs

---
+++ lib/application/notes/note_actor/note_actor_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../domain/notes/entities/note.dart';
+import '../../../domain/notes/i_note_repository.dart';
+import '../../../domain/notes/note_failure.dart';
+
+part 'note_actor_event.dart';
+part 'note_actor_state.dart';
+part 'note_actor_bloc.freezed.dart';
+
+@injectable
+class NoteActorBloc extends Bloc<NoteActorEvent, NoteActorState> {
+  final INoteRepository _noteRepository;
+
+  NoteActorBloc(this._noteRepository) : super(const NoteActorState.initial()) {
+    on<NoteActorEvent>((event, emit) async {
+      if (event is Deleted) {
+        emit(const NoteActorState.actionInProgress());
+
+        final possibleFailure = await _noteRepository.delete(event.note);
+
+        emit(
+          possibleFailure.fold(
+            (failure) => NoteActorState.deleteFailure(failure),
+            (_) => const NoteActorState.deleteSuccess(),
+          ),
+        );
+      }
+    });
+  }
+}
---
+++ lib/application/notes/note_watcher/note_watcher_bloc.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/kt.dart';
+
+import '../../../domain/notes/entities/note.dart';
+import '../../../domain/notes/i_note_repository.dart';
+import '../../../domain/notes/note_failure.dart';
+
+part 'note_watcher_event.dart';
+part 'note_watcher_state.dart';
+part 'note_watcher_bloc.freezed.dart';
+
+@injectable
+class NoteWatcherBloc extends Bloc<NoteWatcherEvent, NoteWatcherState> {
+  final INoteRepository _noteRepository;
+
+  StreamSubscription<Either<NoteFailure, KtList<Note>>>?
+      _noteStreamSubscription;
+
+  NoteWatcherBloc(
+    this._noteRepository,
+  ) : super(const NoteWatcherState.initial()) {
+    on<NoteWatcherEvent>((event, emit) async {
+      if (event is WatchAllStarted) {
+        emit(const NoteWatcherState.loadInProgress());
+
+        _noteStreamSubscription = _noteRepository.watchAll().listen(
+              (failureOrNotes) => add(
+                NoteWatcherEvent.notesReceived(failureOrNotes),
+              ),
+            );
+      } else if (event is WatchUncompletedStarted) {
+        emit(const NoteWatcherState.loadInProgress());
+
+        await _noteStreamSubscription!.cancel();
+
+        _noteStreamSubscription = _noteRepository.watchUncompleted().listen(
+              (failureOrNotes) => add(
+                NoteWatcherEvent.notesReceived(failureOrNotes),
+              ),
+            );
+      } else if (event is NotesReceived) {
+        emit(
+          event.failureOrNotes.fold(
+            (failure) => NoteWatcherState.loadFailure(failure),
+            (notes) => NoteWatcherState.loadSuccess(notes),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  Future<void> close() async {
+    await _noteStreamSubscription!.cancel();
+    return super.close();
+  }
+}
